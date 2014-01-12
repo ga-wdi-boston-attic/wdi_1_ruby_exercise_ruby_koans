396cf58e52b43efcdadc1bd4b150ab5ad4045850
My Name <rlbell@alum.bu.edu>
2014-01-12 11:10:45 -0500

    Complete Ruby Koans, Parallel Assignments

--- koans/about_array_assignment.rb
+++ koans/about_array_assignment.rb
@@ -3,49 +3,49 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    first_name = names.first
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name =  "John"
+    last_name = "Smith"
   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name = "John"
+    last_name = "Smith"
   end
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name = "John"
+    last_name = "Smith", "III"
   end
 
   def test_parallel_assignments_with_too_few_variables
     first_name, last_name = ["Cher"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name = "Cher"
+    last_name = nil
   end
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name = ["Willie", "Rae"]
+    last_name = "Johnson"
   end
 
   def test_parallel_assignment_with_one_variable
     first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    first_name = ["John", "Smith"]
   end
 
   def test_swapping_with_parallel_assignment
     first_name = "Roy"
     last_name = "Rob"
     first_name, last_name = last_name, first_name
-    assert_equal __, first_name
-    assert_equal __, last_name
+    first_name = "Rob"
+    last_name = "Roy"
   end
 end
