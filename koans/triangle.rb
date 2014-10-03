3c2668c8d6cb00d4dc867d21ce291be2bc9c4a1c
fishermanswharff <fishermanswharff@github.com>
2014-10-03 00:03:59 -0400

    up to 184/282

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == 0 && b == 0 && c == 0 
+    raise TriangleError, "You have no sides"
+  end
+  if a < 0 || b < 0 || c < 0
+    raise TriangleError, "You can't have negative sides."
+  end
+  
+  if a + b < c || a + c < b || a + c < b
+    raise TriangleError, "You're so stupid"
+  elsif a + b == c || a + c == b || b + c == a
+    raise TriangleError, "You're so stupid"
+  end
+      
+  if a == b && a == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  elsif a != b || a != c
+    :scalene
+  end
+
+  
 end
 
 # Error class used in part 2.  No need to change this code.
--- tmp/testing_grounds.rb
+++ tmp/testing_grounds.rb
@@ -1,1 +1,1 @@
-puts "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+puts "one two-three".gsub(/(t\w*)/) { $1[0, 1] }`
