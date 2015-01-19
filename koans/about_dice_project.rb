87f89472715d053fb54aadc0350a32b2cf4d0883
J.Marx <jennyrutledge@gmail.com>
2015-01-19 17:42:08 -0500

    Complete classes, dice_project, and open_classes koans.

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -5,6 +5,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_reader :values
+
+  def roll(num)
+    @values = num.times.map{ rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
